f8daeff92c71d01ad159fc270fb60e0b213223cd
basilmirshab <basilmirshabetr@gmail.com>
2023-09-30 21:53:16 +0530

    edits

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+<style>
+@font-face {
+  font-family: 'neopixel';
+  src: url('/assets/font/NEOPIXEL-Regular.otf')  format('opentype');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+html{
+  font-family: 'neopixel';
+}
+</style>
