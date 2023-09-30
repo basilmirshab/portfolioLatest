ea81ac405452df97dbea6670af4e775881249376
basilmirshab <basilmirshabetr@gmail.com>
2023-09-30 18:20:08 +0530

    testing

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-   <h3 class="text-green-500">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse obcaecati rerum quidem hic, repudiandae magni dolore nesciunt illo quisquam consequatur sit alias error voluptatibus in, temporibus fugiat amet est.
-   </h3>
+    <NuxtWelcome />
   </div>
 </template>
