d86df6b6282d61a40d2ca0ec2ff72bf50eeb1a15
basilmirshab <basilmirshabetr@gmail.com>
2023-09-30 18:11:15 +0530

    initial

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+   <h3 class="text-green-500">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, esse obcaecati rerum quidem hic, repudiandae magni dolore nesciunt illo quisquam consequatur sit alias error voluptatibus in, temporibus fugiat amet est.
+   </h3>
+  </div>
+</template>
